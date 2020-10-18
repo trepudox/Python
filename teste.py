93e3f1863d07a5cd395c78e9dffacc43ab4f0703
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-10-18 13:11:07 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,14 +1,5 @@
-quantia_lista = int(input('Digite um número inteiro para definir o valor da lista: '))
-lista = list(range(quantia_lista))
-lista = [i+1 for i in lista]
+lista = [i + 1 for i in range(10)]
 
-dicionario_quadrado = {str(x+1): str(y**2) for x, y in enumerate(lista)}
-entrada = ''
-while True:
-    entrada = input('Digite um número da lista para saber seu valor ao quadrado, para sair digite "!sair": ')
-    if entrada.casefold() == '!sair':
-        break
+dicionario = {x: y for x in lista for y in [0, input(f'Digite o valor da chave {x}:')]}
 
-    print(f'O valor de {entrada} ao quadrado é {dicionario_quadrado.get(entrada)}\n')
-
-print('Fim do programa')
+print(dicionario)
