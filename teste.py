7917428130676a1b004583b58c6c119eb171b2d4
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-10-19 16:33:52 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,23 +1,7 @@
-import time
-
-
-def quadrado(nums):
-    for x in nums:
+def quadrado(lista):
+    for x in lista:
         yield x*x
-        time.sleep(0.5)
 
 
-for i in quadrado(list(range(10))):
-    print(i)
-
-
-
-def quadrado_lista(nums):
-    res = []
-    for x in nums:
-        res.append(x*x)
-
-    return res
-
-
-print(quadrado_lista(list(range(10))))
+for valor in quadrado(list(range(10))):
+    print(valor)
