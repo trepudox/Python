73c4a12816078587a499996b819fbcefa00b6b97
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-10-20 22:35:49 -0300

    gerador de números primos criado

---
+++ projetos/desafios/geradorNumerosPrimos.py
@@ -0,0 +1,28 @@
+import random
+
+
+def e_primo(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    else:
+        return True
+
+
+try:
+    entrada = int(input('Digite o tamanho desejado do número: '))
+    if entrada < 1:
+        raise ValueError('Valor não pode ser menor ou igual a 0')
+finally:
+    pass
+
+while True:
+    numero = str(random.randint(1, 9))
+    for x in range(entrada - 1):
+        numero += str(random.randint(0, 9))
+
+    numero = int(numero)
+
+    if e_primo(numero):
+        print(f'O número {numero} é primo')
+        break
