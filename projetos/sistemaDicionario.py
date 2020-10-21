cd82eff34155b5e444689d6c054475fd97b02d40
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-10-20 21:02:52 -0300

    a

--- projetos/sistemaDicionario.py
+++ projetos/sistemaDicionario.py
@@ -1,16 +1,34 @@
 dic = {'Arroz': 10.0, 'Alface': 20.0, 'Feijão': 999.0}
 condicao = ''
 while True:
-    condicao = input('Digite o que quer fazer, "V" para ver preços, "A" para adicionar produtos, e "!sair" para sair: ')
+    if not condicao == '':
+        print()
+    condicao = input('Digite o que quer fazer, "V" para ver o preço de algum produto específico, "A" para adicionar'
+                     ' produtos, "L" para listar todos os produtos, \n"D" para deletar algum produto, e "!sair" para '
+                     'sair: ')
+
     if condicao.casefold() == '!sair':
         break
+
     elif condicao.casefold() == 'v':
         entrada = input('Digite o produto desejado para saber o preço: ').title()
         print(f'{entrada} custa R$', dic.get(entrada), '\n')
+
     elif condicao.casefold() == 'a':
         produtoadd = str(input('Digite o nome do produto: ')).title()
         dic[produtoadd] = float(input(f'Digite o valor de "{produtoadd}": '))
-        print(f'Agora o dicionário ficou assim: ')
+        print(f'Agora sua lista está assim: ')
         print(dic, '\n')
+
+    elif condicao.casefold() == 'd':
+        try:
+            dic.pop(input('Digite o item que deseja deletar: '))
+        except KeyError:
+            print('Este produto não estava incluso na lista, impossível apagá-lo.')
+
+    elif condicao.casefold() == 'l':
+        for valores in dic.items():
+            print(valores)
+
 
 print('Fim do programa.')
--- teste2.py
+++ teste2.py
@@ -1,2 +1,5 @@
 dic = {'Arroz': 10, 'Alface': 20, 'Feijão': 999}
 
+print(dic)
+dic.pop('Feijã')
+print(dic)
