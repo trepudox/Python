6423daba232cfbf3798e2974d723472800f9c090
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-10-22 01:20:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,8 @@
-lista1 = [1, 2, 3, 4, 5, 6]
+from itertools import zip_longest
+
+lista1 = [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 1234]
 lista2 = [0, 5, 1, 4]
 
-resultado = [a+b for a, b in zip(lista1, lista2)]
+resultado = [a+b for a, b in zip_longest(lista1, lista2, fillvalue=0)]
 
 print(resultado)
