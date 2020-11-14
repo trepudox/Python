440b70cbc44c26baba95f9267a8dbb6898561b7f
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-11-13 23:38:19 -0300

    a

--- teste3.py
+++ teste3.py
@@ -1,66 +1,60 @@
-def iptu(valor, residencial=True):
-    mult, sub = 0, 0
-    valor_negativo = False
+from math import sqrt
 
-    if 0 <= valor < 150000:
-        if residencial:
-            mult = 0.007
-        else:
-            mult = 0.011
-        sub = 0
-    elif 150000 <= valor < 300000:
-        if residencial:
-            mult = 0.009
-        else:
-            mult = 0.013
-        sub = 300
-    elif 300000 <= valor < 600000:
-        if residencial:
-            mult = 0.011
-        else:
-            mult = 0.015
-        sub = 900
-    elif 600000 <= valor < 1200000:
-        if residencial:
-            mult = 0.013
-        else:
-            mult = 0.017
-        sub = 2100
-    elif valor >= 1200000:
-        if residencial:
-            mult = 0.015
-        else:
-            mult = 0.019
-        sub = 4500
-    else:
-        valor_negativo = True
-        print('O valor do imóvel não pode ser negativo.')
-
-    calculo = (valor * mult) - sub
-
-    if valor_negativo:
-        return
-    else:
-        return round(calculo, 1)
+erro = False
 
 
-if __name__ == '__main__':
+def decodifica(criptografada, num):
+    global erro
+    descripto = ''
+    for char in criptografada:
+        try:
+            descripto += chr(ord(char) - num)
+        except ValueError:
+            print('\nHá elementos não criptografados na mensagem. Impossível descriptografar.')
+            erro = True
+            return
+    return descripto
+
+
+print("\nPrograma de descriptografia, insira corretamente a chave e depois o nome do arquivo texto, que tenha uma "
+      "mensagem criptografada pelo programa 'cripto'.\n(O arquivo texto precisa estar no mesmo diretório deste programa"
+      " para que tudo funcione corretamente)", end='\n\n')
+while True:
     try:
-        entrada_tipo = input('Digite "1" caso seu imóvel seja residencial. Digite "2" para demais imóveis: ')
-        if len(entrada_tipo) != 1:
-            raise ValueError
-        entrada_tipo = int(entrada_tipo)
-        if entrada_tipo == 1:
-            entrada = int(input('Digite aqui o valor do seu imóvel: '))
-            if iptu(entrada) is not None:
-                print('\nO valor do seu IPTU é de: ' + str(iptu(entrada)), end='\n\n')
-        elif entrada_tipo == 2:
-            entrada = int(input('Digite aqui o valor do seu imóvel: '))
-            if iptu(entrada, residencial=False) is not None:
-                print('O valor do seu IPTU é de: ' + str(iptu(entrada, residencial=False)), end='\n\n')
+        a = input('Digite a chave A: ')  # 0
+        b = input('Digite a chave B: ')  # 9
+        c = input('Digite a chave C: ')  # 0
+        d = input('Digite a chave D: ')  # 0
+        e = input('Digite a chave E: ')  # 0
+        a, b, c, d, e = int(a), int(b), int(c), int(d), int(e)
     except ValueError:
-        print('\nDigite apenas "1" ou "2" para as opções de imóvel.\n')
-    except TypeError:
-        print('\nO valor do imóvel tem que ser um número inteiro, sem pontos ou alfacaracteres.\n')
+        print('\nChave incorreta.')
+        break
 
-    print('Fim do programa.')
+    delta = (((-b) ** 2 - 4 * a * c) + d) + e
+    chave_correta = delta == 81
+
+    if chave_correta:
+        entrada_arquivo = input('\nDigite o nome do arquivo a ser descriptografado: ').lower()
+        try:
+            with open(f'{entrada_arquivo}.txt', 'r', encoding='UTF-8') as arquivo_txt:
+                leitura = arquivo_txt.read()
+        except FileNotFoundError:
+            print('\nO arquivo de texto não existe!')
+            break
+
+        raiz = int(sqrt(delta))
+
+        mensagem_descriptografada = decodifica(leitura, (int(raiz) - 5) * 100)
+
+        if erro:
+            break
+
+        print('\nA mensagem descriptografada é a seguinte:\n' + mensagem_descriptografada)
+        break
+
+    else:
+        print('\nChave incorreta.')
+        break
+
+print('\nFim do programa.')
