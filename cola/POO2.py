2a8660840ffc1f456a4a8e623042516e95b9d115
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-11-18 16:10:39 -0300

    poo

--- cola/POO2.py
+++ cola/POO2.py
@@ -1,8 +1,12 @@
+from abc import ABC, abstractmethod
+
+
 def pulalinha():
     print()
 
 
-class Eletronico:
+class Eletronico(ABC):
+    @abstractmethod
     def __init__(self, nome):
         self._nome = nome
         self._ligado = False
@@ -49,27 +53,41 @@ class Eletronico:
 
 
 class Celular(Eletronico):
-    def __init__(self, modelo, marca, cor, armazenamento):
+    def __init__(self, modelo, marca, cor, armazenamento, memoria):
         super().__init__(modelo)
         self._marca = marca
         self._cor = cor
         self._armazenamento = armazenamento
+        self._memoria = memoria
 
     def fazer_ligacao(self):
-        print(self._nome, 'está fazendo uma ligação.')
+        if self._ligado:
+            print(self._nome, 'está fazendo uma ligação.')
+        else:
+            print(self._nome, 'está desligado, impossível fazer uma ligação agora.')
+
+        pulalinha()
+
+    def especificacoes(self):
+        print(f'Especificações {self._nome}:\n\tMarca: {self._marca}\n\tModelo: {self._nome}\n\tCor: {self._cor}\n'
+              f'\tArmazenamento: {self._armazenamento} GB\n\tMemória RAM: {self._memoria} GB')
 
         pulalinha()
 
 
+class CelularSamsung(Celular):
+    def __init__(self, modelo, cor, armazenamento, memoria, __marca='Samsung'):
+        super().__init__(modelo, __marca, cor, armazenamento, memoria)
+        self._marca = 'Samsung'
+
+
 if __name__ == '__main__':
-    ele1 = Eletronico('Celular')
-
-    ele1.ligar()
-    ele1.desligar()
-    ele1.tem_bateria()
-    ele1.ligar()
-    ele1.acabar_bateria()
-
-    cel1 = Celular('J8', 'Samsung', 'Preto', 64)
-
+    cel1 = Celular('J8', 'Samsung', 'Preto', 64, 4)
+    cel1.ligar()
     cel1.fazer_ligacao()
+
+    celSamsung1 = CelularSamsung('J8', 'Preto', 64, 4)
+
+    celSamsung1.especificacoes()
+
+    celSamsung1.ligar()
--- teste.py
+++ teste.py
@@ -1,75 +1,16 @@
-def pulalinha():
-    print()
+from abc import ABC, abstractmethod
 
 
-class Eletronico:
-    def __init__(self, nome):
+class Pessoa(ABC):
+    @abstractmethod
+    def __init__(self, nome, idade):
         self._nome = nome
-        self._ligado = False
-        self._sembateria = False
-
-    def ligar(self):
-        if self._ligado:
-            print(f'{self._nome} já está ligado.')
-        else:
-            self._ligado = True
-            print(f'{self._nome} agora está ligado.')
-
-        pulalinha()
-
-    def desligar(self):
-        if self._ligado:
-            self._ligado = False
-            print(f'{self._nome} agora está desligado.')
-        else:
-            print(f'{self._nome} já está desligado.')
-
-        pulalinha()
-
-    def tem_bateria(self):
-        if self._sembateria:
-            print(self._nome, 'está sem bateria.')
-        else:
-            print(self._nome, 'ainda tem bateria.')
-
-        pulalinha()
-
-    def acabar_bateria(self):
-        self._sembateria = True
-        print(self._nome, 'está sem bateria...')
-        self.desligar()
-
-        pulalinha()
-
-    def carregar_bateria(self):
-        self._sembateria = False
-        print(self._nome, 'agora está com a bateria carregada.')
-
-        pulalinha()
+        self._idade = idade
 
 
-class Celular(Eletronico):
-    def __init__(self, modelo, marca, cor, armazenamento):
-        super().__init__(modelo)
-        self._marca = marca
-        self._cor = cor
-        self._armazenamento = armazenamento
-
-    def fazer_ligacao(self):
-        print(self._nome, 'está fazendo uma ligação.')
-
-        pulalinha()
+class Aluno(Pessoa):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
 
 
-if __name__ == '__main__':
-    ele1 = Eletronico('Celular')
-
-    ele1.ligar()
-    ele1.desligar()
-    ele1.tem_bateria()
-    ele1.ligar()
-    ele1.acabar_bateria()
-
-    cel1 = Celular('J8', 'Samsung', 'Preto', 64)
-
-    cel1.fazer_ligacao()
+p1 = Aluno('marco', 18)
