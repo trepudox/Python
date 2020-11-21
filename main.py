669ae4181fc35ed8259a39cf493fbe22b267a359
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-11-21 12:16:24 -0300

    .

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from codigosPython.cola.POO2 import *
+from pythonProject.codigosPython.cola.POO2 import Eletronico, Celular
 
-ele1 = Eletronico('Celular')
+# ele1 = Eletronico('Celular')
 
-ele1.ligar()
-ele1.desligar()
-ele1.tem_bateria()
-ele1.ligar()
-ele1.acabar_bateria()
+# ele1.ligar()
+# ele1.desligar()
+# ele1.tem_bateria()
+# ele1.ligar()
+# ele1.acabar_bateria()
 
-cel1 = Celular('J8', 'Samsung', 'Preto', 64)
+cel1 = Celular('J8', 'Samsung', 'Preto', 64, 4)
 
 cel1.fazer_ligacao()
