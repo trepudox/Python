30a50afa5a51f4f35d305c4c4caf4082c7ec7095
Marco Aurelio Queiroz <teoaa2@gmail.com>
2020-11-22 13:31:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ class App(QMainWindow):
         super().__init__(parent)
         self.cw = QWidget
         self.grid = QGridLayout(self.cw)
-        self.btn = QPushButton('Texto do botão')
+        self.btn1 = QPushButton('Texto do botão')
+        self.btn2 = QPushButton('Outro botão')
 
         self.setCentralWidget(self.cw)
 
-        self.grid.addWidget(self.btn, 0, 0, 1, 1)
-        
+        self.grid.addWidget(self.btn1, 0, 0, 1, 1)
+        self.grid.addWidget(self.btn2, 0, 1, 1, 1)
+
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
