d7b8d0702a3b45af94ec9adeb6ccabedcb468595
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-12-02 04:02:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-entrada = int(input('wentrada: '))
-cont = entrada - 1
-cont2 = 0
-cont3 = -1
-cont4 = ((entrada - 1) * 2) - 1
-
-print(' ' * (entrada - 1) + '#' + ' ' * (entrada - 1))
-
-for x in range(entrada - 1):
-    cont -= 1
-    cont3 += 2
-    print(' ' * cont + '#' + ' ' * cont3 + '#')
-
-for x in range(entrada - 2):
-    cont2 += 1
-    cont4 -= 2
-    print(' ' * cont2 + '#' + ' ' * cont4 + '#')
-
-print(' ' * (entrada - 1) + '#' + ' ' * (entrada - 1))
-
+for index, y in enumerate(range(10)):
+    print(index, y)
