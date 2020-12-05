64d76ef717d829374d70404643de17fdcbd5e81d
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-12-05 02:01:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-for index, y in enumerate(range(10)):
-    print(index, y)
+def for_e_indice(string: str):
+    for indice, x in enumerate(string):
+        print(str(indice) + ':', x)
+
+
+for_e_indice('string teste')
+
+lta = [x**2 for x in range(1,11)]
+print(lta)
