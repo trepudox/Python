26d9f9104e25b76a757dc9a0c9d2ff0d7a57d4ab
Marco Aurélio Queiroz <teoaa2@gmail.com>
2020-12-29 05:21:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,22 @@
-lista = []
-condicao = True
+entrada = input()
+pontos = ['!', ',', '.', '?', ';', ':']
+for x in pontos:
+    entrada = entrada.replace(x, '')
 
+palavras = entrada.split(' ')
 
-def pulalinha():
-    print()
+soma = 0
+contador = 0
 
+for plv in palavras:
+    soma += len(str(plv))
+    contador += 1
+else:
+    media = soma / contador
 
-def add():
-    try:
-        nome = input('Nome: ')
-        valor = int(input('Valor: '))
-        lista.append((nome, valor))
-    except ValueError:
-        print('\nValor precisa ser um número\n')
+media_for_inteira = soma % contador == 0
 
-    pulalinha()
-
-
-def rmv():
-    try:
-        entrada = int(input('Número do item a ser removido: '))
-        lista.pop(entrada)
-    except IndexError:
-        print('\nNão há nenhum item com este índice.')
-    except ValueError:
-        print('\nApenas números são validos.')
-
-    pulalinha()
-
-
-def ver():
-    if len(lista) == 0:
-        print('\nLista vazia.')
-    print()
-    for indice, item in enumerate(lista):
-        print('Item', str(indice) + ':', item[0] + ',', item[1])
-    pulalinha()
-
-
-def sair():
-    global condicao
-    print('\nFim do programa.')
-    condicao = False
-
-
-def com():
-    pulalinha()
-    print('"add" - adiciona itens na lista')
-    print('"rmv" - remove itens da lista de acordo com a sua posição')
-    print('"ver" - mostra a lista')
-    print('"sair" - sai do programa')
-    print('"com" - mostra os comandos')
-    pulalinha()
-
-
-print('Digite "com" para ver os comandos.\n')
-
-while condicao:
-    escolha = input('Digite o comando que quer executar: ').lower()
-
-    if escolha == 'add':
-        add()
-
-    elif escolha == 'rmv':
-        rmv()
-
-    elif escolha == 'ver':
-        ver()
-
-    elif escolha == 'com':
-        com()
-
-    elif escolha == 'sair':
-        sair()
-
-    else:
-        print()
-        print('Comando inexistente')
-        print()
-
-
-with open('arquivo.txt', 'w') as arquivo:
-    for indice, item in enumerate(lista):
-        arquivo.write(f"Item {indice}: {item[0]}, {item[1]}")
+if media_for_inteira:
+    print(int(media))
+else:
+    print(int(media) + 1)
