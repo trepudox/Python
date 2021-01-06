9aa98e1747c266bda0c7bbec98c10a7956c14fcd
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-01-06 11:06:34 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-entrada = input()
+string = [1, 2, 3, 4, 5, 6456, 12324, 7543]
 
-cassino = entrada.replace('x', '')
+for x in string:
+    print(chr(x))
+    if x > 1000:
+        print(1001)
+        for y in range(1, 1002):
+            print(y)
 
-print(cassino)
-
+        break
